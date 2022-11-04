2be99167dbeb809a59686497b59a496ed96413ac
nguyenxuanthuan200 <nguyenxuanthuan200@gmail.com>
2022-11-04 22:12:41 +0700

    AddBalaceAccount API

--- ShopSecondHand/Data/RequestModels/CheckOutRequest/CartListProduct.cs
+++ ShopSecondHand/Data/RequestModels/CheckOutRequest/CartListProduct.cs
@@ -5,8 +5,6 @@ namespace ShopSecondHand.Data.RequestModels.CheckOutRequest
     public class CartListProduct
     {
         public Guid Id { get; set; }
-        public double Total { get; set; }
         public int Quantity { get; set; }
-        public double Price { get; set; }
     }
 }
